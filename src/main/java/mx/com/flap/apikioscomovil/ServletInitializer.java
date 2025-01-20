df3b4ab5dc88581439bc7d02b3803f644a0f406e
Edgar Molina <edgar.martinez@flap.com.mx>
2025-01-20 16:57:16 -0600

    Replace test class with Servlet support and WAR packaging.
    Removed the unused `ApiKioscoMovilApplicationTests` class and added `ServletInitializer` to enable deployment as a servlet. Updated `pom.xml` to set the project packaging to WAR and adjusted `application.properties` configurations.

---
+++ src/main/java/mx/com/flap/apikioscomovil/ServletInitializer.java
@@ -0,0 +1,12 @@
+package mx.com.flap.apikioscomovil;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApiKioscoMovilApplication.class);
+    }
+}
